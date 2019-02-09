98950222c580bcfe2fb463db9499911220a3a3c7
AlexCraftAC <34915443+AlexCraftAC@users.noreply.github.com>
2019-02-09 15:22:17 +0200

    Update shop.php

--- shop.php
+++ shop.php
@@ -1,5 +1,5 @@
 <?php
 $content = file_get_contents('https://shop.lazylinks.fr/');
 $content = str_replace('</title>','</title><base href="https://shop.lazylinks.fr/" />', $content);
-$content = str_replace('</head>','<link rel="stylesheet" href="https://raw.githubusercontent.com/AlexCraftAC/fns-shop/master/shop.css" /></head>', $content);
+$content = str_replace('</head>','<link rel="stylesheet" href="http://alexcraft.ga/fns-shop/shop.css" /></head>', $content);
 echo $content;
