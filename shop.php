2659cbd672f0ebaded11afa229e1d5a85c8c6b83
AlexCraftAC <34915443+AlexCraftAC@users.noreply.github.com>
2019-02-15 18:19:21 +0200

    Delete shop.php

--- shop.php
+++
@@ -1,5 +0,0 @@
-<?php
-$content = file_get_contents('https://shop.lazylinks.fr/');
-$content = str_replace('</title>','</title><base href="https://shop.lazylinks.fr/" />', $content);
-$content = str_replace('</head>','<link rel="stylesheet" href="http://alexcraft.ga/fns-shop/shop.css" /></head>', $content);
-echo $content;
