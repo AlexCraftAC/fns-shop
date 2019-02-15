7cfe06646ecd18b526c7aeedd3c1a8c5c69fbfaa
AlexCraftAC <34915443+AlexCraftAC@users.noreply.github.com>
2019-02-15 18:22:33 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,3 +1,3 @@
 <?php
-	define( 'FN_API_KEY', 'YOUR-API-KEY-HERE' );
+	define( 'https://fortnite-api.tresmos.xyz/profile/pc/tresmos?key=', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1vdXNhbGxhbWFsZXhhbmRyb3NAZ21haWwuY29tIiwidXNlcklkIjoiNWMyY2M5N2E3OGNmMTgwZmI4MTNkZGUzIn0.QUxvDeNqQ_xP-RzETmkt4hYhjL0hG7RNyCwsE6WxJfg' );
 ?>
