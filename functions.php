964e058d2d4ca7ed8cfa360901c77890191eeaf9
AlexCraftAC <34915443+AlexCraftAC@users.noreply.github.com>
2019-02-15 18:23:51 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -14,7 +14,7 @@
 	 */
 	function getStoreDataFromAPI( $date ) {
 		// store api endpoint, we hit those endpoints!
-		$apiUrlEndpointStore = 'https://api.fortnitetracker.com/v1/store';
+		$apiUrlEndpointStore = 'https://fortnite-api.tresmos.xyz/profile/pc/tresmos?key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1vdXNhbGxhbWFsZXhhbmRyb3NAZ21haWwuY29tIiwidXNlcklkIjoiNWMyY2M5N2E3OGNmMTgwZmI4MTNkZGUzIn0.QUxvDeNqQ_xP-RzETmkt4hYhjL0hG7RNyCwsE6WxJfg';
 
 		// curl setup
 		$ch = curl_init();
