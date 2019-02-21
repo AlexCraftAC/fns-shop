6cf471b58bca1e0fbf9be61230f55b6f38b86e26
AlexCraftAC <34915443+AlexCraftAC@users.noreply.github.com>
2019-02-21 15:41:08 +0200

    Delete config.php

--- config.php
+++
@@ -1,3 +0,0 @@
-<?php
-	define( 'https://fortnite-api.tresmos.xyz/profile/pc/tresmos?key=', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1vdXNhbGxhbWFsZXhhbmRyb3NAZ21haWwuY29tIiwidXNlcklkIjoiNWMyY2M5N2E3OGNmMTgwZmI4MTNkZGUzIn0.QUxvDeNqQ_xP-RzETmkt4hYhjL0hG7RNyCwsE6WxJfg' );
-?>
