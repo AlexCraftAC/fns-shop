b5cdd123de90a57f22da93750932cb96ec5293ae
AlexCraftAC <34915443+AlexCraftAC@users.noreply.github.com>
2019-02-22 14:45:08 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
-$.getJSON('https://coolapis.ga/shop.php', function(data) {
-    $.each(data.shop.item, function(i,f) {
-        $("ul").append("<li>URL: "+f.gallery+"</li>");
-    });
+$(document).ready(frunction() {
+  $.getJSON('https://coolapis.ga/shop.php', function(data) {
+      $.each(data.shop.item, function(i,f) {
+          $("ul").append("<img src=" + f.gallery + ">");
+      });
+   });
 });
