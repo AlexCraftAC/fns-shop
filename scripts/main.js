10e1e455721e740adfd3d61d9ca6ad387e10f527
AlexCraftAC <34915443+AlexCraftAC@users.noreply.github.com>
2019-02-22 14:56:09 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-$(document).ready(frunction() {
+function renderItems(pReset=true) {
   $.getJSON('https://coolapis.ga/shop.php', function(data) {
       $.each(data.shop.item, function(i,f) {
           $("#images").append("<img src=" + f.gallery + ">");
