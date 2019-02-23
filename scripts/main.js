4e68d0c470ea30621a2c85b7f0e80b058abf172a
AlexCraftAC <34915443+AlexCraftAC@users.noreply.github.com>
2019-02-23 20:18:55 +0200

    Delete main.js

--- scripts/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $.getJSON('https://coolapis.ga/shop.php', function(data) {
-      $.each(data.shop.item, function(i,f) {
-          $("#images").append("<img src=" + f.gallery + ">");
-      });
-   });
-});
